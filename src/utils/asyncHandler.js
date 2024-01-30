f0169172300ce9be496b52cdebc5b77daafc71c7
Ayush <ayushsaxena917@gmail.com>
2024-01-30 12:54:58 +0530

    some utils codes

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/*
+const asyncHandler = (func) => {};
+const asyncHandler = (func) => {() => {}};
+const asyncHandler = (func) => async() => {}
+// function which has another func as a argument or it returns a func is called higher order func
+
+*/
+
+/*    
+const asyncHandler = (func) => async (req, res, next) => {
+  try {
+    await (req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+*/
